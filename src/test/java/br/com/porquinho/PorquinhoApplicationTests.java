326d58adfcc4d03600d10ee318c1551565a463b5
Guilherme P. Silva <guilherme.silva@sefaz.mt.gov.br>
2025-09-30 17:08:46 -0400

    first

---
+++ src/test/java/br/com/porquinho/PorquinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.porquinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorquinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
