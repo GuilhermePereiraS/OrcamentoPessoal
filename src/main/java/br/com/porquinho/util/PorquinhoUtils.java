1d652f78e373819ed2e7da38e4fde07bd9f8b467
guilherme <g.pereira@.com>
2025-11-11 21:51:02 -0400

    adicionado criaErro

--- src/main/java/br/com/porquinho/util/PorquinhoUtils.java
+++ src/main/java/br/com/porquinho/util/PorquinhoUtils.java
@@ -1,5 +1,7 @@
 package br.com.porquinho.util;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 public class PorquinhoUtils {
 
     public static boolean estaVazioOuNulo(String string) {
@@ -8,6 +10,12 @@ public class PorquinhoUtils {
 
     public static boolean temCaracterEspecial(String texto) {
         return texto.matches(".*[^a-zA-Z0-9].*");
+    }
+
+    public static void criaErro(RedirectAttributes redirectAttributes, String mensagemErro) {
+        redirectAttributes.addFlashAttribute("alerta", true);
+        redirectAttributes.addFlashAttribute("mensagemAlerta", mensagemErro);
+        redirectAttributes.addFlashAttribute("iconeAlerta", "error");
     }
 
 
